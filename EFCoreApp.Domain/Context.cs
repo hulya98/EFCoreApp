9637dcdab394c650f5b005298d20c9297847eecb
ApertechProjects <info@apertech.net>
2024-08-12 17:56:27 +0400

    currency entity added

---
+++ EFCoreApp.Domain/Context.cs
@@ -0,0 +1,38 @@
+﻿using EFCoreApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreApp.Domain
+{
+    public class Context : DbContext
+    {
+
+        public Context()
+        {
+
+        }
+
+        public DbSet<Currency> Currencies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>().HasData(new Currency
+            {
+                CurrencyId = 1,
+                CurrencyKey = "AZN",
+                CurrencyName = "Azərbaycan manatı"
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+             //optionsBuilder.UseSqlServer()
+        }
+    }
+}
---
+++ EFCoreApp.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreApp.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public int? CreatedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
